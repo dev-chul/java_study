50b3e7a66ee623abe02cf024b1143f107f71373b
lostjcwide <lostjcwide@naver.com>
2021-10-20 21:26:15 +0900

    adding comment...

--- src/com/code/sample4/fss/Sample.java
+++ src/com/code/sample4/fss/Sample.java
@@ -12,16 +12,30 @@ public class Sample {
 	 */
 	public static void main(String[] args) {
 		Graph g = new Graph(9);
-		g.addEdge(0, 1);
-		g.addEdge(1, 2);
-		g.addEdge(1, 3);
-		g.addEdge(2, 4);
-		g.addEdge(2, 3);
-		g.addEdge(3, 4);
-		g.addEdge(3, 5);
-		g.addEdge(5, 6);
-		g.addEdge(5, 7);
+		g.addEdge(0, 1); //
+		g.addEdge(1, 2); // 
+		g.addEdge(1, 3); //
+		g.addEdge(2, 4); //
+		g.addEdge(2, 3); //
+		g.addEdge(3, 4); //
+		g.addEdge(3, 5); //
+		g.addEdge(5, 6); //
+		g.addEdge(5, 7); //
 		g.addEdge(6, 8);
+		
+		/*
+		 * adjacent가 각 점의 관계도를 의미한다.
+		 * nodes's index	adjacent's Node List
+		 * 0 				n1 
+		 * 1				n0 n2 n3 
+		 * 2				n1 n4 n3 
+		 * 3				n1 n2 n4 n5 
+		 * 4				n2 n3 
+		 * 5				n3 n6 n7 
+		 * 6				n5 n8 
+		 * 7				n5 
+		 * 8				n6 
+		 */
 		
 		/*
 		 * DFS stack vs Recursion(재귀호출)의 차이
